86234ffb0837d47b03d8dce419a326924b09503b
ALAN <alan.morais@etec.sp.gov.br>
2023-07-08 11:22:18 -0300

    08/07/2023 aula Java

---
+++ Pessoa.java
@@ -0,0 +1,75 @@
+import javax.swing.JOptionPane;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private float peso;
+    private double altura;
+
+    public Pessoa(){
+        Object parentComponent;
+        String message;
+        setNome(
+            JOptionPane.showInputDialog(
+                null, "Insira o nome")
+                .toString()
+        );
+        setIdade(Integer.parseInt(
+            JOptionPane.showInputDialog(null, "Insira a idade")
+            
+        ));
+            setPeso(Float.parseFloat(
+                JOptionPane.showInputDialog(null ,"Insira o peso")
+                
+            ));
+            setAltura(Float.parseFloat(
+                JOptionPane.showInputDialog( null,"Insira a altura")
+                )
+            );
+
+    }
+
+    public Pessoa(String name, int idade,
+    float peso, double altura){
+        setNome(nome);
+        setIdade(idade);
+        setPeso(peso);
+        setAltura(altura);
+    }
+    public String getName(){
+        return this.nome;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
